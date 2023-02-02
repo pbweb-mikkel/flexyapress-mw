ad686359654000b8eb7a316a8e09c735b7d38735
Mikkel Pindstofte <mikkel@pbweb.dk>
2023-02-02 15:01:46 +0100

    mobile changes

--- flexyapress-mw.php
+++ flexyapress-mw.php
@@ -1,5 +1,5 @@
 <?php
-define( 'FLEXYAPRESS_VERSION', '1.24' );
+define( 'FLEXYAPRESS_VERSION', '1.25' );
 /**
  * The plugin bootstrap file
  *
@@ -16,7 +16,7 @@ define( 'FLEXYAPRESS_VERSION', '1.24' );
  * Plugin Name:       PB Web - Mindworking
  * Plugin URI:        https://pbweb.dk/flexyapress
  * Description:       Dette plugin sørger for dataintegration med Mindworking.
- * Version:           1.24
+ * Version:           1.25
  * Author:            PB Web
  * Author URI:        https://pbweb.dk
  * License:           GPL-2.0+
